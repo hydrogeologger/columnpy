8d4ea59eac624b6711ae13f6a5a648a73e38c6d6
diablo465 <cmzhang1318@hotmail.com>
2016-07-27 09:57:57 +1000

    add python files

---
+++ python/constants.py
@@ -0,0 +1,55 @@
+#http://stackoverflow.com/questions/2682745/how-to-create-a-constant-in-python
+g2kg = 0.001
+kg2g = 1000.
+ms2mmday=3600.*24*1000;
+kelvin=273.15;
+mm2m=0.001;
+m2mm=1000.;
+rhow_pure_water=1000;
+g=9.8;
+second2day=1./3600/24;
+kg2g=1000.;
+g2kg=0.001;
+#class constants:
+#    g2kg = 0.001
+#    kg2g = 1000.
+#    ms2mmday=3600.*24*1000;
+#    kelvin=273.15;
+#    mm2m=0.001;
+#    m2mm=1000.;
+#    rhow_pure_water=1000;
+#    g=9.8;
+#    second2day=1./3600/24;
+#    kg2g=1000.;
+#    g2kg=0.001;
+#    #self.psim_log_range=-[0.0001:0.0001:0.001,0.002:0.001:0.01,0.02:0.01:0.1,0.2:0.1:1,2:1:10,20:10:100,200:100:1000,2000:1000:10000,20000:10000:1e5...
+#    #	,2e5:1e5:1e6,2e6:1e6:9e6,1e7:1e7:9e7,1e8:1e8:9e8];
+#    mol_weight_water=0.018; #kg/mol
+#    R =8.314;
+#    def __init__(self):
+#        self.g2kg = 0.001
+#        self.kg2g = 1000.
+#        self.ms2mmday=3600.*24*1000;
+#        self.kelvin=273.15;
+#        self.mm2m=0.001;
+#        self.m2mm=1000.;
+#        self.rhow_pure_water=1000;
+#        self.g=9.8;
+#        self.second2day=1./3600/24;
+#        self.kg2g=1000.;
+#        self.g2kg=0.001;
+#        #self.psim_log_range=-[0.0001:0.0001:0.001,0.002:0.001:0.01,0.02:0.01:0.1,0.2:0.1:1,2:1:10,20:10:100,200:100:1000,2000:1000:10000,20000:10000:1e5...
+#        #	,2e5:1e5:1e6,2e6:1e6:9e6,1e7:1e7:9e7,1e8:1e8:9e8];
+#        self.mol_weight_water=0.018; #kg/mol
+#        self.R =8.314;
+#    def area(self):
+#        return self.x * self.y
+#    def perimeter(self):
+#        return 2 * self.x + 2 * self.y
+#    def describe(self,text):
+#        self.description = text
+#    def authorName(self,text):
+#        self.author = text
+#    def scaleSize(self,scale):
+#        self.x = self.x * scale
+#        self.y = self.y * scale
