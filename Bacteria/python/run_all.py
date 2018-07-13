45518c440aec6dacf25a00bc2d43112471daff86
osboxes.org <osboxes@osboxes>
2018-07-13 02:04:28 -0400

    trial

--- Bacteria/python/run_all.py
+++ Bacteria/python/run_all.py
@@ -4,4 +4,5 @@ execfile('python/init_pandas_mo_su.py')
 execfile('python/read_schedule.py')
 # this is a milestone done at 2018-05-02
 execfile('python/plot_bacteria.py')
+execfile('python/plot_suction_calibration_result.py')
 
