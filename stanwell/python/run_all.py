817a5f02df74189a127f4c50996316578091734a
osboxes.org <osboxes@osboxes>
2019-04-03 19:50:14 -0400

    add python

---
+++ stanwell/python/run_all.py
@@ -0,0 +1,12 @@
+execfile('python/init_pandas.py')
+execfile('python/init_pandas_mo_su.py')
+# init_pandas_weather_two_stations.py is working only before 180522
+#execfile('python/init_pandas_weather_two_stations_dl.py')
+execfile('python/init_pandas_weather_two_stations_dl.py')
+execfile('python/read_schedule.py')
+# this is a milestone done at 2018-05-02
+
+#execfile('python/plot_weather_daily.py')
+execfile('python/plot_weather.py')
+execfile('python/plot_stanwell.py')
+
