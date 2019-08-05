4b04235209970eaef41bad0a89fb83e77d9a5a1c
osboxes.org <osboxes@osboxes>
2019-08-04 21:40:46 -0400

    try data

---
+++ qal/python/run_all_update.py
@@ -0,0 +1,12 @@
+#execfile('python/pull_data2.py')
+execfile('python/init_pandas.py')
+
+execfile('python/init_pandas_mo_su.py')
+execfile('python/init_pandas_weather_two_stations.py')
+execfile('python/read_schedule_update_June2019.py')
+# this is a milestone done at 2018-05-02
+execfile('python/plot_qal_modify.py')
+execfile('python/plot_weather.py')
+execfile('python/plot_water_balance_qal.py')
+#execfile('python/plot_qal1807_sch.py')
+
