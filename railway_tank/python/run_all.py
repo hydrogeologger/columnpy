d647834170a1d40e4107e67cd569b0e8a1aa86a2
XimingLei <leiximing0217@gmail.com>
2021-01-22 01:37:47 -0500

    add railway_tank

---
+++ railway_tank/python/run_all.py
@@ -0,0 +1,5 @@
+exec(open('python/get_data_post_processing.py').read()
+#-----crop photos for creating videos
+#exec(open('python/crop_img').read()
+exec(open('python/plot_pics.py').read()
+exec(open('python/create_video.py').read()
