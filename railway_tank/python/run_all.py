60d10d25720c44c90712c4df8b1b4638d0e229e1
XimingLei <leiximing0217@gmail.com>
2021-01-22 01:35:48 -0500

    delete railway_tank

--- railway_tank/python/run_all.py
+++
@@ -1,5 +0,0 @@
-exec(open('python/get_data_post_processing.py').read()
-#-----crop photos for creating videos
-#exec(open('python/crop_img').read()
-exec(open('python/plot_pics.py').read()
-exec(open('python/create_video.py').read()
